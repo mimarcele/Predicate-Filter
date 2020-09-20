bdb129c2dc449a8261f2e247023d805d4807d7d7
Milena Gomes <milena.gomes.silva@everis.com>
2020-09-19 23:27:52 -0300

    Adicionando classe papelaria com alguns atributos

---
+++ src/Treinando/Papelaria.java
@@ -0,0 +1,14 @@
+package Treinando;
+
+public class Papelaria {
+
+    final String objeto;
+    final double preco;
+    final boolean estaNaLista;
+
+    public Papelaria(String objeto, double preco, boolean estaNaLista) {
+        this.objeto = objeto;
+        this.preco = preco;
+        this.estaNaLista = estaNaLista;
+    }
+}
