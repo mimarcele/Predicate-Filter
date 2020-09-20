94bbf570148db0cfd5ea4468202eb2b2d59c565a
Milena Gomes <milena.gomes.silva@everis.com>
2020-09-19 23:24:20 -0300

    Adicionando classe para servir de base para testes

---
+++ src/Treinando/Carros.java
@@ -0,0 +1,16 @@
+package Treinando;
+
+public class Carros {
+    final String nome;
+    final double preco;
+    final String marca;
+    final int lugares;
+
+    public Carros(String nome, double preco, String marca, int lugares){
+        this.nome = nome;
+        this.preco = preco;
+        this.marca = marca;
+        this.lugares = lugares;
+    }
+
+}
